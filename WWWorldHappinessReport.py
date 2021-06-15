c1782b4dc8ca24c5b094d94093fa38283989e3b0
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-06-15 08:48:51 +0300

    Update WWWorldHappinessReport.py

--- WWWorldHappinessReport.py
+++ WWWorldHappinessReport.py
@@ -13,3 +13,16 @@ def createdataframe(filename):
     """
     return pd.read_csv(filename)
 
+def ladderscoreofeverycontinent(dataframe):
+    indexlist = dataframe["Regional indicator"].unique().tolist()
+    for i in indexlist:
+        dataframe[dataframe["Regional indicator"]==str(i)].plot(kind='barh', x="Country name", y="Ladder score", figsize=(10,15))
+        plt.savefig("plots/Vaccinations of "+str(i)+".png")
+
+def main():
+    df = createdataframe(FILENAME)
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
