bd2eeffb737e189a65fefc6af60ad2fdb66c0650
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-07-08 10:08:45 +0300

    Update WorldHappiness.py

--- WorldHappiness.py
+++ WorldHappiness.py
@@ -2,32 +2,32 @@ from tkinter import Tk, Menu
 from tkinter import messagebox as msg
 
 class WorldHappiness():
-    def __init__(self,master):
+    def __init__(self, master):
         self.master = master
         self.master.title("World Happiness")
         self.master.geometry("250x200")
-        self.master.resizable(False,False)
+        self.master.resizable(False, False)
         
         self.menu = Menu(self.master)
         
-        self.file_menu = Menu(self.menu,tearoff = 0)
+        self.file_menu = Menu(self.menu, tearoff=0)
         self.file_menu.add_command(label="Insert a csv", accelerator='Ctrl+O', command=self.insertcsv)
         self.file_menu.add_command(label="Close File", accelerator="Ctrl+F4", command=self.closefile)
-        self.file_menu.add_command(label="Exit", accelerator= 'Alt+F4',command = self.exitmenu)
-        self.menu.add_cascade(label = "File",menu=self.file_menu)
+        self.file_menu.add_command(label="Exit", accelerator='Alt+F4', command=self.exitmenu)
+        self.menu.add_cascade(label="File", menu=self.file_menu)
         
-        self.about_menu = Menu(self.menu,tearoff = 0)
-        self.about_menu.add_command(label = "About",accelerator= 'Ctrl+I',command=self.aboutmenu)
-        self.menu.add_cascade(label="About",menu=self.about_menu)
+        self.about_menu = Menu(self.menu, tearoff=0)
+        self.about_menu.add_command(label="About", accelerator='Ctrl+I', command=self.aboutmenu)
+        self.menu.add_cascade(label="About", menu=self.about_menu)
         
-        self.help_menu = Menu(self.menu,tearoff = 0)
-        self.help_menu.add_command(label = "Help",accelerator = 'Ctrl+F1',command=self.helpmenu)
-        self.menu.add_cascade(label="Help",menu=self.help_menu)
+        self.help_menu = Menu(self.menu, tearoff=0)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
+        self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         self.master.config(menu=self.menu)
-        self.master.bind('<Alt-F4>',lambda event: self.exitmenu())
-        self.master.bind('<Control-F1>',lambda event: self.helpmenu())
-        self.master.bind('<Control-i>',lambda event: self.aboutmenu())
+        self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
+        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
+        self.master.bind('<Control-i>', lambda event: self.aboutmenu())
 
     
     def insertcsv(self):
@@ -36,9 +36,8 @@ class WorldHappiness():
     def closefile(self):
         pass
 
-
-    
     def exitmenu(self):
+        """ Exit function"""
         if msg.askokcancel("Quit?", "Really quit?"):
             self.master.destroy()
     
@@ -51,9 +50,10 @@ class WorldHappiness():
         
 
 def main():
-    root=Tk()
+    """menu function"""
+    root = Tk()
     WorldHappiness(root)
     root.mainloop()
     
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
