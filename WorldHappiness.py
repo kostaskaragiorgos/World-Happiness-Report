085c59c4e4775038686a6c9b7e205696f16f3ccd
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2021-07-09 13:54:34 +0300

    Update WorldHappiness.py

--- WorldHappiness.py
+++ WorldHappiness.py
@@ -5,6 +5,9 @@ from tkinter import messagebox as msg
 def aboutmenu():
     msg.showinfo("ABOUT", "WORLD HAPPINESS REPORT\nVERSION 1.0")
 
+def helpmenu():
+    msg.showinfo("HELP", "INSERT A WORLD HAPPINESS REPORT")
+
 class WorldHappiness():
     def __init__(self, master):
         self.master = master
@@ -25,13 +28,13 @@ class WorldHappiness():
         self.menu.add_cascade(label="About", menu=self.about_menu)
         
         self.help_menu = Menu(self.menu, tearoff=0)
-        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=self.helpmenu)
+        self.help_menu.add_command(label="Help", accelerator='Ctrl+F1', command=helpmenu)
         self.menu.add_cascade(label="Help", menu=self.help_menu)
         
         self.master.config(menu=self.menu)
         self.master.bind('<Alt-F4>', lambda event: self.exitmenu())
-        self.master.bind('<Control-F1>', lambda event: self.helpmenu())
-        self.master.bind('<Control-i>', lambda event: self.aboutmenu())
+        self.master.bind('<Control-F1>', lambda event: helpmenu())
+        self.master.bind('<Control-i>', lambda event: aboutmenu())
 
     
     def insertcsv(self):
@@ -45,8 +48,7 @@ class WorldHappiness():
         if msg.askokcancel("Quit?", "Really quit?"):
             self.master.destroy()
     
-    def helpmenu(self):
-        pass
+
     
 
 
